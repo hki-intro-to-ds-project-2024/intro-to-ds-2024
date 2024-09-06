6b8d882e89b1d2dfe0c9305304a90799c49bb720
Miika Matias <miikapiiparinen24@gmail.com>
2024-09-06 14:01:55 +0300

    add poetry-based flask backend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,18 @@
-import flask
+from flask import Flask, send_file, send_from_directory
+from pathlib import Path
 
+FRONTEND_DIR = Path(__file__).parent.parent / "frontend/dist/"
+DEVELOPMENT_ENV = True
+
+app = Flask("bicycle-map-app")
+
+@app.route("/")
+def index():
+    return send_file(FRONTEND_DIR / "index.html")
+
+@app.route("/<path:path>")
+def file_paths(path):
+    return send_from_directory(FRONTEND_DIR,path)
 
 if __name__ == "__main__":
-    app()
+    app.run(debug=DEVELOPMENT_ENV)
