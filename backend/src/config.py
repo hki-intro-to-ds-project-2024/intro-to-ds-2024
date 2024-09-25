aab3ed2cf3ef33bd7ff6d2d42f8e81410fff7b5c
Miika Matias <miikapiiparinen24@gmail.com>
2024-09-25 15:11:22 +0300

    add liikennemaarat data

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-DATA_DIR = Path(__file__).parent / ".." / "data"
+DATA_DIR = Path(__file__).parent / ".." / ".." / "data"
 FRONTEND_DIR = Path(__file__).parent.parent.parent / "frontend/dist/"
 
 DEVELOPMENT_ENV = True
---
+++ docs/data_info.md
@@ -0,0 +1,8 @@
+# Data info
+This document contains a paragraph for each directory in backend/data
+
+### rides_2021
+
+### stops
+
+### trafficff
