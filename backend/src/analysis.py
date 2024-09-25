bc81d6ba62f1ed9a3d2687fa5e3f8c0ea7508c8f
- <->
2024-09-25 15:48:23 +0300

    backend: read stops into nodes

--- backend/src/analysis.py
+++ backend/src/analysis.py
@@ -15,25 +15,14 @@ if __name__ == "__main__":
 
     
     data_rides = pd.read_csv(DATA_DIR / "rides_2021/2021-04.csv")
-    print(data_rides)
+    #print(data_rides)
 
     data_stops = pd.read_csv(DATA_DIR / "stops/Helsingin_ja_Espoon_kaupunkipyöräasemat_avoin.csv")
-    print(data_stops)
-    
-    for row in data_stops: 
-        print(row)
-        #longitude = row['x']
-        #latitude  =  row['y']
-        #node = Node(longitude, latitude)
-        #analytics.nodes.append(node)
-    
+    print(data_stops)    
 
-    latlongs = [(60.192059, 24.945831), (60.191, 25), (60.192059, 24.945831)]
-
-    for latlong in latlongs: 
-        longitude = latlong[0]
-        latitude  = latlong[1]
-        node = Node(longitude, latitude)
-        analytics.nodes.append(node)
+    nodes = [Node(x, y) for x, y in zip(data_stops['x'], data_stops['y'])]
+    analytics.nodes = nodes
 
     print(analytics.nodes)
+
+    print(len(analytics.nodes))
