c3be2ecf6b6ad0efd0bb7db091b70644e5d7dad8
Miika Matias <miikapiiparinen24@gmail.com>
2024-09-27 11:42:08 +0300

    frontend: add getData

---
+++ frontend/src/node-data.tsx
@@ -0,0 +1,28 @@
+type NodeNata = Array<{
+    id: string;
+    position: google.maps.LatLngLiteral;
+    type: 'pin' | 'html';
+    zIndex: number;
+  }>;
+  
+function getData() {
+    const data: NodeNata = [];
+  
+    // create 50 random markers
+    for (let index = 0; index < 50; index++) {
+      data.push({
+        id: String(index),
+        position: {lat: rnd(53.52, 53.63), lng: rnd(9.88, 10.12)},
+        zIndex: index,
+        type: Math.random() < 0.5 ? 'pin' : 'html'
+      });
+    }
+  
+    return data;
+  }
+  
+  function rnd(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+  }
+
+export default getData;
