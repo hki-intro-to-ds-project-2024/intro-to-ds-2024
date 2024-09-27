3fd39d609d7cee7f00525fbf4443b78d0c4e335b
Miika Matias <miikapiiparinen24@gmail.com>
2024-09-27 11:26:48 +0300

    frontend: prune drawing-manager

--- frontend/src/use-drawing-manager.tsx
+++ frontend/src/use-drawing-manager.tsx
@@ -2,7 +2,7 @@ import {useMap, useMapsLibrary} from '@vis.gl/react-google-maps';
 import {useEffect, useState} from 'react';
 
 export function useDrawingManager(
-  initialValue: google.maps.drawing.DrawingManager | null = null
+    initialValue: google.maps.drawing.DrawingManager | null = null
 ) {
   const map = useMap();
   const drawing = useMapsLibrary('drawing');
@@ -10,49 +10,29 @@ export function useDrawingManager(
   const [drawingManager, setDrawingManager] =
     useState<google.maps.drawing.DrawingManager | null>(initialValue);
 
-  useEffect(() => {
-    if (!map || !drawing) return;
+    useEffect(() => {
+        if (!map || !drawing) return;
 
-    // https://developers.google.com/maps/documentation/javascript/reference/drawing
-    const newDrawingManager = new drawing.DrawingManager({
-      map,
-      drawingMode: google.maps.drawing.OverlayType.MARKER,
-      drawingControl: true,
-      drawingControlOptions: {
-        position: google.maps.ControlPosition.TOP_CENTER,
-        drawingModes: [
-          google.maps.drawing.OverlayType.MARKER,
-          google.maps.drawing.OverlayType.CIRCLE,
-          google.maps.drawing.OverlayType.POLYGON,
-          google.maps.drawing.OverlayType.POLYLINE,
-          google.maps.drawing.OverlayType.RECTANGLE
-        ]
-      },
-      markerOptions: {
-        draggable: true
-      },
-      circleOptions: {
-        editable: true
-      },
-      polygonOptions: {
-        editable: true,
-        draggable: true
-      },
-      rectangleOptions: {
-        editable: true,
-        draggable: true
-      },
-      polylineOptions: {
-        editable: true,
-        draggable: true
-      }
-    });
+        // https://developers.google.com/maps/documentation/javascript/reference/drawing
+        const newDrawingManager = new drawing.DrawingManager({
+        map,
+        drawingMode: google.maps.drawing.OverlayType.MARKER,
+        drawingControl: true,
+        drawingControlOptions: {
+            drawingModes: [
+            google.maps.drawing.OverlayType.MARKER,
+            ]
+        },
+        markerOptions: {
+            draggable: true
+        },
+        });
 
-    setDrawingManager(newDrawingManager);
+        setDrawingManager(newDrawingManager);
 
-    return () => {
-      newDrawingManager.setMap(null);
-    };
+        return () => {
+        newDrawingManager.setMap(null);
+        };
   }, [drawing, map]);
 
   return drawingManager;
