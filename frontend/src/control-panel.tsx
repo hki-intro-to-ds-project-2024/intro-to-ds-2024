3f91b5ff45c472d3708eb7492a8b828f06b00ef5
Miika Matias <miikapiiparinen24@gmail.com>
2024-10-04 15:33:20 +0300

    frontend: add rudimentary control-panel

--- frontend/src/control-panel.tsx
+++ frontend/src/control-panel.tsx
@@ -3,21 +3,30 @@ import * as React from 'react';
 function ControlPanel() {
   return (
     <div className="control-panel">
-      <h3>Helsinki Bicycle Map Project</h3>
+      <h3>Welcome to Biketainer!</h3>
       <p>
-        Click on the map to add an additional bike stop and see how it affects the other edges.
+        Use the parameters in the input box to find at-risk bike stands for breakage. 
       </p>
-      <div className="links">
-        <a
-          href="https://codesandbox.io/s/github/visgl/react-google-maps/tree/main/examples/drawing"
-          target="_new">
-          Try on CodeSandbox ↗
-        </a>
 
+      <label htmlFor="minimum-zero-rides">Minimum number of zero-rides</label>
+      <input id="minimum-zero-rides" type="number"></input>
+      <div></div>
+      <label htmlFor="minimum-proportion">Minimum proportion of zero-rides</label>
+      <input id="minimum-proportion" type="number" step="0.01"></input>
+      <div></div>
+      <label htmlFor="date-start">Date start (YYYY-MM-DD)</label>
+      <input id="date-start" type="date"></input>
+      <div></div>
+      <label htmlFor="date-end">Date end (YYYY-MM-DD)</label>
+      <input id="date-end" type="date"></input>
+      <div></div>
+      <button type="button">Update Map</button>
+
+      <div className="links" style={{float: 'right', marginRight: '0em', marginTop: '-1.5em', position: 'absolute', right: 0, zIndex: 1000}}>
         <a
-          href="https://github.com/visgl/react-google-maps/tree/main/examples/drawing"
+          href="https://github.com/hki-intro-to-ds-project-2024/intro-to-ds-2024"
           target="_new">
-          View Code ↗
+          View Source Code ↗
         </a>
       </div>
     </div>
