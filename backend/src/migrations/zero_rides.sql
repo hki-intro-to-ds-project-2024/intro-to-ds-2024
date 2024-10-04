83a0b0c8e4e9543bb434c3fd8c5fadf2250f9c36
Miika Matias <miikapiiparinen24@gmail.com>
2024-10-04 13:59:11 +0300

    backend: modify db schema

--- backend/src/migrations/zero_rides.sql
+++ backend/src/migrations/zero_rides.sql
@@ -1,5 +1,7 @@
-CREATE TABLE stops( time TIMESTAMPTZ NOT NULL DEFAULT now(),
+CREATE TABLE stops(time TIMESTAMPTZ NOT NULL DEFAULT now(),
                         lat DOUBLE PRECISION,
-                        lng DOUBLE PRECISION);
+                        lng DOUBLE PRECISION,
+                        zero_rides INTEGER
+                        total_rides INTEGER);
 
 SELECT create_hypertable('stops', by_range('time'));
