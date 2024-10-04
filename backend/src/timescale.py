12f4d64dcfc67edb465512d91490202378ed1cf2
Miika Matias <miikapiiparinen24@gmail.com>
2024-10-04 14:02:31 +0300

    backend: randomize stop zero_rides total_rides

--- backend/src/timescale.py
+++ backend/src/timescale.py
@@ -1,5 +1,6 @@
 import psycopg2
 import os
+from random import randint
 from src.config import MIGRATIONS_DIR, TIMESCALE_CONN_STRING
 from src.node import Node
 
@@ -9,11 +10,10 @@ class TimescaleClient:
         self.conn = psycopg2.connect(TIMESCALE_CONN_STRING)
         self.cur = self.conn.cursor()
 
-    def add_node(self, node: Node) -> None:
+    def add_stop(self, node: Node) -> None:
         self.cur.execute(
-            "INSERT INTO nodes (lat, lng) VALUES (%s, %s)",
-            (node.coords[0], node.coords[1])
-        )
+            "INSERT INTO stops (lat, lng, zero_rides, total_rides) VALUES (%s, %s, %s, %s)",
+            (node.coords[0], node.coords[1], randint(0,10), randint(10, 100)))
         self.conn.commit()
 
     def apply_schema(self, schema_file: str) -> None:
