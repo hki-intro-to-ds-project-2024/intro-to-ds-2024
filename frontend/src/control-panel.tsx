e03c6057c38981e1cd8b3cf07316febd39a8aeae
Hamid Aebadi <hamidaebadi@yahoo.com>
2024-10-06 11:10:49 +0300

    Implement query request from backend

--- frontend/src/control-panel.tsx
+++ frontend/src/control-panel.tsx
@@ -1,6 +1,36 @@
 import * as React from 'react';
+import axios from 'axios'
+
 
 function ControlPanel() {
+  const [zero_values, set_zeros] = React.useState(0)
+  const [zero_proportions, set_proportions] = React.useState(0.0)
+  const [start_data, set_start_date] = React.useState()
+  const [start_time, set_start_time] = React.useState()
+  const [end_date, set_end_date] = React.useState()
+  const [end_time, set_end_time] = React.useState()
+
+  const handle_zeros_value = (event) => set_zeros(event.target.value)
+  const handle_proportion = (event) => {
+    set_proportions(event.currentTarget.value)
+    document.getElementById('minimum-proportion-value')!.innerHTML = event.currentTarget.value
+  }
+
+  const handle_start_date = (event) => set_start_date(event.target.value)
+  const handle_start_time = (event) => set_start_time(event.target.value)
+  const handle_end_date = (event) => set_end_date(event.target.value)
+  const handle_end_time = (event) => set_end_time(event.target.value)
+
+
+  //handle data submition
+  const submit_parameters = () => {
+    //get data from server
+    const url = "http://127.0.0.1:5000/nodes?time_start="+start_data+"T"+start_time+"&time_end="+end_date+"T"+end_time
+    +"&zero_rides="+zero_values+"&proportion="+zero_proportions
+
+    axios.get(url).then(response => console.log(response.data))
+  }
+  
   return (
     <div className="control-panel">
       <h3>Welcome to Biketainer!</h3>
@@ -12,7 +42,7 @@ function ControlPanel() {
         <tbody>
           <tr>
             <td><label htmlFor="minimum-zero-rides">Minimum number of zero-rides</label></td>
-            <td><input id="minimum-zero-rides" type="number" step="1"></input></td>
+            <td><input id="minimum-zero-rides" type="number" step="1" value={zero_values} onChange={handle_zeros_value}/></td>
           </tr>
           <tr>
             <td><label htmlFor="minimum-proportion">Minimum proportion of zero-rides</label></td>
@@ -23,7 +53,8 @@ function ControlPanel() {
                 min="0"
                 max="1"
                 step="0.01"
-                onChange={e => (document.getElementById('minimum-proportion-value')!.innerHTML = e.currentTarget.value)}
+                value={zero_proportions}
+                onChange={handle_proportion}
               />
               <span id="minimum-proportion-value">1</span>
             </td>
@@ -31,20 +62,20 @@ function ControlPanel() {
           <tr>
             <td><label htmlFor="date-start">Date start (YYYY-MM-DD HH:MM:SS)</label></td>
             <td>
-              <input id="date-start-date" type="date"></input>
-              <input id="date-start-time" type="time"></input>
+              <input id="date-start-date" type="date" value={start_data} onChange={handle_start_date} />
+              <input id="date-start-time" type="time" value={start_time} onChange={handle_start_time}/>
             </td>
           </tr>
           <tr>
             <td><label htmlFor="date-end">Date end (YYYY-MM-DD HH:MM:SS)</label></td>
             <td>
-              <input id="date-end-date" type="date"></input>
-              <input id="date-end-time" type="time"></input>
+              <input id="date-end-date" type="date" value={end_date} onChange={handle_end_date}></input>
+              <input id="date-end-time" type="time" value={end_time} onChange={handle_end_time}></input>
             </td>
           </tr>
         </tbody>
       </table>
-      <button type="button">Update Map</button>
+      <button type="button" onClick={submit_parameters}>Update Map</button>
 
       <div className="links" style={{float: 'right', marginRight: '0em', marginTop: '-1.5em', position: 'absolute', right: 0, zIndex: 1000}}>
         <a
